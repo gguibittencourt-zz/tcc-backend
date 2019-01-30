42320ff89d0bde34a726f226ec10c5e1046f24d0
Guilherme Silva <guilherme.silva@digilab.com.br>
2019-01-30 16:23:12 -0200

    wip expected result

---
+++ src/main/java/br/com/spring/jersey/dto/ExpectedResult.java
@@ -0,0 +1,50 @@
+package br.com.spring.jersey.dto;
+
+import java.util.Objects;
+
+public class ExpectedResult {
+
+    private String idExpectedResult;
+    private String name;
+    private String description;
+
+
+    public String getIdExpectedResult() {
+        return idExpectedResult;
+    }
+
+    public void setIdExpectedResult(String idExpectedResult) {
+        this.idExpectedResult = idExpectedResult;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ExpectedResult)) return false;
+        ExpectedResult that = (ExpectedResult) o;
+        return Objects.equals(getIdExpectedResult(), that.getIdExpectedResult()) &&
+                Objects.equals(getName(), that.getName()) &&
+                Objects.equals(getDescription(), that.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getIdExpectedResult(), getName(), getDescription());
+    }
+}
