6f7290f0f7066c6921eb1c682a0051429afa092b
Guilherme Bittencourt da Silva <gguibittencourt@gmail.com>
2019-09-20 00:24:40 -0300

    wip

--- src/main/java/br/com/tcc/dto/Rating.java
+++ src/main/java/br/com/tcc/dto/Rating.java
@@ -5,6 +5,9 @@ import java.util.Objects;
 public class Rating {
     private String id;
     private String name;
+    private String mappedName;
+    private Float maxValue;
+    private Float minValue;
 
     public String getId() {
         return id;
@@ -24,17 +27,47 @@ public class Rating {
         return this;
     }
 
+    public String getMappedName() {
+        return mappedName;
+    }
+
+    public Rating setMappedName(String mappedName) {
+        this.mappedName = mappedName;
+        return this;
+    }
+
+    public Float getMaxValue() {
+        return maxValue;
+    }
+
+    public Rating setMaxValue(Float maxValue) {
+        this.maxValue = maxValue;
+        return this;
+    }
+
+    public Float getMinValue() {
+        return minValue;
+    }
+
+    public Rating setMinValue(Float minValue) {
+        this.minValue = minValue;
+        return this;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Rating)) return false;
-        Rating that = (Rating) o;
-        return Objects.equals(getId(), that.getId()) &&
-                Objects.equals(getName(), that.getName());
+        Rating rating = (Rating) o;
+        return Objects.equals(getId(), rating.getId()) &&
+                Objects.equals(getName(), rating.getName()) &&
+                Objects.equals(getMappedName(), rating.getMappedName()) &&
+                Objects.equals(getMaxValue(), rating.getMaxValue()) &&
+                Objects.equals(getMinValue(), rating.getMinValue());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getName());
+        return Objects.hash(getId(), getName(), getMappedName(), getMaxValue(), getMinValue());
     }
 }
